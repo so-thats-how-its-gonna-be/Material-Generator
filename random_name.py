0616bd040bbf4033b026b5f2b323ced2b0cd6af4
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2022-10-28 13:34:25 -0500

    tkinter

--- random_name.py
+++ random_name.py
@@ -1,10 +1,11 @@
 import random
 import os
-import tkinter as tk
+import tkinter as ttk
+from tkinter import CENTER, NE, S, Canvas, Frame, Grid, PhotoImage, Tk, font
 import util
 from pathlib import Path
 
-from PIL import Image
+from PIL import Image, ImageTk, ImageTransform
 
 def rch(ch):
     return random.choice(ch)
@@ -39,4 +40,13 @@ img.save('instance.png')
 util.grayscalify('instance.png')
 util.colorify('instance.png')
 
-Image.open('instance.png').show(output)
+root = Tk()
+frm = Frame(root, padx=10, pady=10)
+frmimg = ImageTk.PhotoImage(Image.open('instance.png').copy().resize([128, 128], 0))
+can = Canvas(frm, width=500, height=500)
+can.create_image(250, 250, image=frmimg, anchor=CENTER)
+can.grid(column=1, row=1)
+frm.grid()
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+ttk.Label(frm, text=output.capitalize(), font=font.BOLD).grid(column=1, row=0)
+root.mainloop()
