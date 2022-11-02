f336adfa6f349a84be9fd9aeb35a07d12b5f636a
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2022-11-02 08:07:18 -0500

    more silly properties

--- material.py
+++ material.py
@@ -11,16 +11,18 @@ class Material:
     - Color: {self.color}"""
     
     def blockof(self, blocksprite: str):
-        return MaterialBlock(f"Block Of {self.name}", blocksprite, self.color)
+        return MaterialBlock(f"Block Of {self.name}", blocksprite, self.color, self)
 
 class MaterialBlock:
-    def __init__(self, name: str, parentblockspritepath: str, color: tuple):
+    def __init__(self, name: str, parentblockspritepath: str, color: tuple, parentmaterial: Material):
         self.name = name
         self.blocksprite = parentblockspritepath
         self.color = color
+        self.parentmaterial = parentmaterial
     
     def __str__(self):
         return f"""
 {self.name}:
-    - Block Sprite: {self.blocksprite}
-    - Color: {self.color}"""
+    - Block Sprite Path: {self.blocksprite}
+    - Color: {self.color}
+    - Parent Material: {self.parentmaterial.name}"""
