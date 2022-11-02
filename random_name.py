ede85cf04fb427110e6e1583e9b8741f9fe7fa44
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2022-11-02 15:22:46 -0500

    huge rewrite
    Update random_name.py

--- random_name.py
+++ random_name.py
@@ -1,67 +1,73 @@
-import random
+from random import choice as rch, randint as rint
 import os
-from re import match
 import tkinter as ttk
 from tkinter import CENTER, Canvas, Frame, Tk
-from material import Material, MaterialBlock
+from material import Material
 import util
 
 from PIL import Image, ImageTk
 
-#TODO: more words :)
-wordlist = [
-    ["o", "bre", "a", "lu", "har", "blu", "con", "rab", "alu", "zin", "ma", "ar", "po", "sa", "jo", "rho", "ena", "pi"], 
-    ["b", "man", "n", "mon", "min", "len", "rom", "eg", "lo", "po"], 
-    ["and", "s", "act", "san", "ad", "c", "atr"], 
-    ["ium", "inite", "ite", "in", "on", "ony", "ar", "en"]]
+wordlist = [[], [], [], []]
+for i, line in enumerate(open('wordlist.txt', 'r').readlines()):
+    wordlist[i] = line.strip().split(' ')
 
-match random.randint(0, 2):
-    case 0:
-        output = util.rch(wordlist[0]) + util.rch(wordlist[1]) + util.rch(wordlist[2]) + util.rch(wordlist[3])
-    case 1:
-        output = util.rch(wordlist[0]) + util.rch(wordlist[1]) + util.rch(wordlist[3])
-    case 2:
-        output = util.rch(wordlist[0]) + util.rch(wordlist[2]) + util.rch(wordlist[3])
+matfile = 'material-instance.png'
+blockfile = 'block-instance.png'
 
-output = output.capitalize()
+def main():
+    
+    matname = ''
+    
+    match rint(0, 2):
+        case 0:
+            matname = rch(wordlist[0]) + rch(wordlist[1]) + rch(wordlist[2]) + rch(wordlist[3])
+        case 1:
+            matname = rch(wordlist[0]) + rch(wordlist[1]) + rch(wordlist[3])
+        case 2:
+            matname = rch(wordlist[0]) + rch(wordlist[2]) + rch(wordlist[3])
 
-matreferences = os.getcwd() + "/material-references/"
+    matname = matname.capitalize()
 
-matimg = Image.open(matreferences + util.rch(os.listdir(matreferences))).copy().save('instance.png')
-util.grayscalify('instance.png')
+    matreferences = os.path.join(os.getcwd(), "material-references", "")
 
-R = random.randint(-200, 200)
-G = random.randint(-200, 200)
-B = random.randint(-200, 200)
-util.colorify('instance.png', R, G, B)
-util.spin('instance.png')
+    Image.open(matreferences + rch(os.listdir(matreferences))).copy().save(matfile)
+    util.grayscalify(matfile)
 
-blockreferences = os.getcwd() + "/block-references/"
+    RGB = (rint(-150, 150), rint(-150, 150), rint(-150, 150))
+    util.tint(matfile, RGB)
+    #Spin is disabled for now because the textures it generates are suboptimal.
+    #util.spin(matfile)
+    #Flip is disabled for now because the textures it generates have incorrect lighting.
+    #util.flip(matfile, 0)
 
-blockimg = Image.open(blockreferences + util.rch(os.listdir(blockreferences))).copy().save('block-instance.png')
-util.grayscalify('block-instance.png')
+    blockreferences = os.path.join(os.getcwd(), "block-references", "")
 
-util.colorify('block-instance.png', R*0.5, G*0.5, B*0.5)
+    Image.open(blockreferences + rch(os.listdir(blockreferences))).copy().save(blockfile)
+    util.grayscalify(blockfile)
 
-root = Tk()
-frm = Frame(root, padx=10, pady=10)
+    util.tint(blockfile, tuple([i / 2 for i in RGB]))
 
-ttk.Label(frm, text=output).grid(column=1, row=0)
-frmimg = ImageTk.PhotoImage(Image.open('instance.png').copy().resize([128, 128], 0))
-can = Canvas(frm, width=500, height=500)
-can.create_image(250, 250, image=frmimg, anchor=CENTER)
-can.grid(column=1, row=1)
+    root = Tk()
+    frm = Frame(root, padx=10, pady=10)
 
-ttk.Label(frm, text=f"Block Of {output}").grid(column=1, row=2)
-frmblimg = ImageTk.PhotoImage(Image.open('block-instance.png').copy().resize([128, 128], 0))
-canbl = Canvas(frm, width=500, height=500)
-canbl.create_image(250, 250, image=frmblimg, anchor=CENTER)
-canbl.grid(column=1, row=3)
+    ttk.Label(frm, text=matname).grid(column=1, row=0)
+    frmimg = ImageTk.PhotoImage(Image.open(matfile).copy().resize([128, 128], 0))
+    can = Canvas(frm, width=500, height=500)
+    can.create_image(250, 250, image=frmimg, anchor=CENTER)
+    can.grid(column=1, row=1)
 
-frm.grid()
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
-root.mainloop()
+    ttk.Label(frm, text=f"Block Of {matname}").grid(column=1, row=2)
+    frmblimg = ImageTk.PhotoImage(Image.open(blockfile).copy().resize([128, 128], 0))
+    canbl = Canvas(frm, width=500, height=500)
+    canbl.create_image(250, 250, image=frmblimg, anchor=CENTER)
+    canbl.grid(column=1, row=3)
 
-testmat = Material(output, "instance.png", (R, G, B))
-print(testmat)
-print(testmat.blockof("block-instance.png"))
+    frm.grid()
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=5)
+    root.mainloop()
+
+    testmat = Material(matname, matfile, RGB)
+    print(testmat)
+    print(testmat.blockof(blockfile))
+
+main()
--- util.py
+++ util.py
@@ -1,10 +1,7 @@
-from random import randint, choice
+from random import randint as rint
 from tkinter import Image
 from PIL import Image
-import math
-
-def rch(ch):
-    return choice(ch)
+from math import floor as fl
 
 def grayscalify(path: str):
     """Make an image grayscale.
@@ -14,31 +11,42 @@ def grayscalify(path: str):
     """
     Image.open(path).convert('LA').save(path)
 
-def colorify(path: str, R: float, G: float, B: float):
-    """Tint an image a color with RGB values.
+def tint(path: str, RGB: tuple):
+    """Tint an image a color with RGB values. Floors RGB values to integers.
 
     Args:
         path (str): Path of the file to tint.
-        R (float): Red value of RGB.
-        G (float): Green value of RGB.
-        B (float): Blue value of RGB.
+        RGB (tuple): RGB values to tint the image with.
     """
     image = Image.open(path).convert('RGBA')
     output = []
     for pix in image.getdata():
+        #If the alpha value is 0, then the pixel is transparent, so we don't want to change it.
         if pix[3] in list(range(1, 256)):
-            output.append((pix[0] + math.floor(R), pix[1] + math.floor(G), pix[2] + math.floor(B)))
+            output.append((pix[0] + fl(RGB[0]), pix[1] + fl(RGB[1]), pix[2] + fl(RGB[2])))
         else:
             output.append(pix)
     image.putdata(output)
     image.save(path)
 
-def spin(path: str):
+def spin(path: str, chance: int=3):
     """Randomly rotate an image with a chance to do nothing.
 
     Args:
         path (str): Path of the file to randomly rotate.
+        chance (int, optional): Chance to do nothing, for example, a chance of 1 would cause this function to do nothing 50% of the time. Defaults to 3.
     """
-    if(randint(0, 2)==0):
+    if rint(0, chance) == 0 and chance != 0:
         return
-    Image.open(path).transpose(randint(0, 4)).save(path)
+    Image.open(path).transpose(rint(2, 4)).save(path)
+
+def flip(path: str, chance: int=3):
+    """Randomly flip an image with a chance to do nothing.
+
+    Args:
+        path (str): Path of the file to randomly flip.
+        chance (int, optional): Chance to do nothing, for example, a chance of 1 would cause this function to do nothing 50% of the time. Defaults to 3.
+    """
+    if rint(0, chance) == 0 and chance != 0:
+        return
+    Image.open(path).transpose(0).save(path)
